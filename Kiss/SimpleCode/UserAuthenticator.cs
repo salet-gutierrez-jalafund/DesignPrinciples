410601b229b9c4d31e538e19f0b043745c49af09
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-07-23 22:42:57 -0400

    feat: implement simplified IsValidUser method in UserAuthenticator in SimpleCode folder

---
+++ Kiss/SimpleCode/UserAuthenticator.cs
@@ -0,0 +1,17 @@
+﻿namespace KissSimple;
+
+public class UserAuthenticator
+{
+    private readonly UserDataProvider _userDataProvider;
+
+    public UserAuthenticator(UserDataProvider userDataProvider)
+    {
+        _userDataProvider = userDataProvider;
+    }
+
+    public bool IsValidUser(string username, string password)
+    {
+        var user = _userDataProvider.GetUserByUsername(username);
+        return user != null && user.Password == password;
+    }
+}
