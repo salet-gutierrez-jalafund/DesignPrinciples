4604592683a08c7d84373359c2492acb9e610f8b
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-07-23 23:18:13 -0400

    feat: add Reservation class; add Reservation class with necessary data in SimpleCode folder

---
+++ Yagni/SimpleCode/Reservation.cs
@@ -0,0 +1,15 @@
+﻿namespace YagniSimple;
+
+public class Reservation
+{
+    public string CustomerName { get; set; }
+    public DateTime ReservationDate { get; set; }
+    public int NumberOfPeople { get; set; }
+
+    public Reservation(string customerName, DateTime reservationDate, int numberOfPeople)
+    {
+        CustomerName = customerName;
+        ReservationDate = reservationDate;
+        NumberOfPeople = numberOfPeople;
+    }
+}
